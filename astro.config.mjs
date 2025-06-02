c8266789c64b439e3ecee187baca8c71c568590e
Mustafa Ozan Sindi <ozan.sindi@vuvy.com>
2025-06-03 02:05:07 +0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+const isProd = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   site: 'https://mosindi.github.io/personal-website-astro/',
+  base: isProd ? "/personal-website-astro/" : "/", // ✅ sadece prod için base ver
   integrations: [mdx(), sitemap(), tailwind()],
-  middleware: true, // bu satırı ekle
   output: "static",
-  base: "/personal-website-astro/", // Örn: "/personal-website/"
 });
-
